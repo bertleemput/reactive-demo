389f03834a07d57221786477590bd1de52b5d51e
Bert Leemput <bert.leemput@vanderlande.com>
2020-04-28 17:23:46 +0200

    Adds a few operators to the demo

--- apps/demo/src/app/operators-page/operators-page.component.ts
+++ apps/demo/src/app/operators-page/operators-page.component.ts
@@ -1,12 +1,80 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+import { timer, fromEvent, combineLatest, merge } from "rxjs";
+import { scan, mapTo, withLatestFrom } from "rxjs/operators";
 
 @Component({
   selector: "reactive-demo-operators-page",
   templateUrl: "./operators-page.component.html",
-  styleUrls: ["./operators-page.component.css"]
+  styleUrls: ["./operators-page.component.scss"]
 })
-export class OperatorsPageComponent implements OnInit {
-  constructor() {}
+export class OperatorsPageComponent {
+  combineLatestDemoCode = `
+  const timer$ = timer(0, 1000);
+  const click$ = fromEvent(document, "mousedown").pipe(
+    scan(acc => acc + 1, 0)
+  );
 
-  ngOnInit(): void {}
+  combineLatest([timer$, click$]).subscribe(([timerTicks, clicks]) =>
+    console.log(
+      \`We have received \${timerTicks} timer ticks and \${clicks} clicks\`
+    )
+  );
+  `;
+
+  mergeDemoCode = `
+  const timerOne$ = timer(3000).pipe(mapTo("A"));
+  const timerTwo$ = timer(1000).pipe(mapTo(1));
+  const timerThree$ = timer(2000).pipe(mapTo({ name: "Rxjs Demo" }));
+
+  merge(timerOne$, timerTwo$, timerThree$).subscribe(value =>
+    console.log(value)
+  );
+  `;
+
+  withLatestFromDemoCode = `
+  const timer$ = timer(5000, 1000);
+
+  const click$ = fromEvent(document, "mousedown").pipe(
+    scan(acc => acc + 1, 0)
+  );
+
+  click$
+    .pipe(withLatestFrom(timer$))
+    .subscribe(([click, t]) => console.log(click, t));
+  `;
+
+  startCombineLatestDemo() {
+    const timer$ = timer(0, 1000);
+    const click$ = fromEvent(document, "mousedown").pipe(
+      scan(acc => acc + 1, 0)
+    );
+
+    combineLatest([timer$, click$]).subscribe(([timerTicks, clicks]) =>
+      console.log(
+        `We have received ${timerTicks} timer ticks and ${clicks} clicks`
+      )
+    );
+  }
+
+  startMergeDemo() {
+    const timerOne$ = timer(3000).pipe(mapTo("A"));
+    const timerTwo$ = timer(1000).pipe(mapTo(1));
+    const timerThree$ = timer(2000).pipe(mapTo({ name: "Rxjs Demo" }));
+
+    merge(timerOne$, timerTwo$, timerThree$).subscribe(value =>
+      console.log(value)
+    );
+  }
+
+  startWithLatestFromDemo() {
+    const timer$ = timer(5000, 1000);
+
+    const click$ = fromEvent(document, "mousedown").pipe(
+      scan(acc => acc + 1, 0)
+    );
+
+    click$
+      .pipe(withLatestFrom(timer$))
+      .subscribe(([click, t]) => console.log(click, t));
+  }
 }
